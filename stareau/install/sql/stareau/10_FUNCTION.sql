327ad252d8cc507abb936d7f7f8ddda20c0dcd6d
rldhont <rldhont@gmail.com>
2026-02-16 20:08:08 +0100

    Define functions and triggers for geometry precision and links between canalisation and noeud_reseau

--- stareau/install/sql/stareau/10_FUNCTION.sql
+++ stareau/install/sql/stareau/10_FUNCTION.sql
@@ -2,11 +2,6 @@
 -- PostgreSQL database dump
 --
 
-
-
-
-
-
 SET statement_timeout = 0;
 SET lock_timeout = 0;
 
@@ -19,9 +14,381 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
+CREATE FUNCTION stareau.get_current_setting(setting_name text, default_value text, value_type text DEFAULT 'text') RETURNS text
+    LANGUAGE plpgsql
+    AS $$
+DECLARE
+    setting_value text;
+BEGIN
+    -- Get the setting value, if not set default_value
+    setting_value = coalesce(current_setting(setting_name, true), default_value);
+    -- Try to cast the setting value to the expected type, if it fails return the default_value
+    BEGIN
+        IF value_type = 'integer' THEN
+           RETURN setting_value::integer;
+        ELSIF value_type = 'boolean' THEN
+           RETURN setting_value::boolean;
+        ELSIF value_type = 'real' THEN
+           RETURN setting_value::boolean;
+        END IF;
+    EXCEPTION WHEN OTHERS THEN
+        IF value_type = 'integer' THEN
+           RETURN default_value::integer;
+        ELSIF value_type = 'boolean' THEN
+           RETURN default_value::boolean;
+        ELSIF value_type = 'real' THEN
+           RETURN default_value::boolean;
+        END IF;
+    END;
+
+    RETURN setting_value;
+END;
+$$;
+
+COMMENT ON FUNCTION stareau.get_current_setting(text, text, text) IS
+'Get a PostgreSQL current setting, with a default value if the setting is not set or is invalid.
+The function is used to avoid repeating the coalesce(current_setting(...))::TYPE, 0) = 1 and to
+have a single point of maintenace for getting settings.
+';
+
+-- aa_before_geometry_insert_or_update_reduce_precision()
+CREATE FUNCTION stareau.aa_before_geometry_insert_or_update_reduce_precision() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$
+BEGIN
+    -- Trigger disabled by session variable
+    IF stareau.get_current_setting('stareau.graph.disable.trigger', '0', 'boolean')
+    THEN
+        RETURN NEW;
+    END IF;
+
+    -- Do not modify the geometry if geom field has not been changed
+    IF TG_OP = 'UPDATE' AND (
+            ST_Equals(OLD.geom, NEW.geom)
+            OR
+            ST_Equals(NEW.geom, ST_ReducePrecision(NEW.geom, 0.05))
+        )
+    THEN
+        RETURN NEW;
+    END IF;
+
+    -- Reduce geometry precision
+    NEW.geom = ST_ReducePrecision(NEW.geom, 0.05);
+
+    RETURN NEW;
+END;
+$$;
+
+
+-- FUNCTION aa_before_geometry_insert_or_update()
+COMMENT ON FUNCTION stareau.aa_before_geometry_insert_or_update_reduce_precision() IS
+'Fonction qui arrondit la précision des coordonnées à 0.05 soit (cm si ce n''est pas déjà fait
+lors d''une création ou d''une modification de géométrie.
+Elle est préfixée par aa_ pour être lancée avant les autres trigger,
+car l''ordre alphabétique compte.
+';
+
+-- before_canalisation_insert_or_update()
+CREATE FUNCTION stareau.before_canalisation_insert_or_update() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$
+DECLARE
+    start_point geometry(point);
+    end_point geometry(point);
+    is_aep boolean;
+    upstream_node record;
+    downstream_node record;
+    raise_notice text;
+BEGIN
+    -- Trigger disabled by session variable
+    IF stareau.get_current_setting('stareau.graph.disable.trigger', '0', 'boolean')
+    THEN
+        RETURN NEW;
+    END IF;
+
+    -- Check if we must log
+    raise_notice = stareau.get_current_setting('stareau.graph.raise.notice', '0', 'boolean');
+
+    -- Do nothing if geometry has not changed
+    IF TG_OP = 'UPDATE' AND ST_Equals(NEW.geom, OLD.geom) THEN
+        IF raise_notice IN ('info', 'debug') THEN
+            RAISE NOTICE '% BEFORE canalisation % n° %, NEW and OLD geom are equal',
+                repeat('    ', pg_trigger_depth()::integer), TG_OP, NEW.fid
+            ;
+        END IF;
+
+        RETURN NEW;
+    END IF;
+
+    -- start & end point
+    start_point = ST_StartPoint(NEW.geom);
+    end_point = ST_EndPoint(NEW.geom);
+    -- is reseau AEP ?
+    is_aep = (NEW.type_reseau = 'aep');
+
+    -- Get first nodes < 0.1 m - If found, edit NEW geom
+    -- upstream
+    IF is_aep THEN
+        SELECT INTO upstream_node
+            n.fid, n.id_noeud_reseau, n.geom
+        FROM "stareau_principale".noeud_reseau AS n
+        WHERE ST_DWithin(n.geom, start_point, 0.1)
+          AND n.type_reseau = 'aep'
+        ORDER BY n.geom <-> start_point, n.fid
+        LIMIT 1
+        ;
+    ELSE
+        SELECT INTO upstream_node
+            n.fid, n.id_noeud_reseau, n.geom
+        FROM "stareau_principale".noeud_reseau AS n
+        WHERE ST_DWithin(n.geom, start_point, 0.1)
+          AND n.type_reseau <> 'aep'
+        ORDER BY n.geom <-> start_point, n.fid
+        LIMIT 1
+        ;
+    END IF;
+
+    -- upstream - update value
+    IF upstream_node IS NOT NULL THEN
+        IF raise_notice IN ('info', 'debug') THEN
+            RAISE NOTICE '% BEFORE canalisation % n° %, upstream_node NOT NULL : % % -> use it',
+                repeat('    ', pg_trigger_depth()::integer), TG_OP, NEW.fid, upstream_node.fid, upstream_node.id_noeud_reseau
+            ;
+        END IF;
+
+        -- Update the geometry
+        NEW.geom = ST_SetPoint(NEW.geom, 0, upstream_node.geom);
+        -- Update the node ID in upstream attribute
+        NEW.noeudinitial = upstream_node.id_noeud_reseau;
+    ELSE
+        IF raise_notice IN ('info', 'debug') THEN
+            RAISE NOTICE '% BEFORE canalisation % n° %, upstream_node IS NULL',
+                repeat('    ', pg_trigger_depth()::integer), TG_OP, NEW.fid
+            ;
+        END IF;
+
+        -- Update the node ID in upstream attribute
+        NEW.noeudinitial = 'non_renseigne';
+    END IF;
+
+    -- Get last nodes < 0.1 m - If found, edit NEW geom
+    -- downstream
+    IF is_aep THEN
+        SELECT INTO downstream_node
+            n.fid, n.id_noeud_reseau, n.geom
+        FROM "stareau_principale".noeud_reseau AS n
+        WHERE ST_DWithin(n.geom, end_point, 0.1)
+          AND n.type_reseau = 'aep'
+        ORDER BY n.geom <-> end_point, n.fid
+        LIMIT 1
+        ;
+    ELSE
+        SELECT INTO downstream_node
+            n.fid, n.id_noeud_reseau, n.geom
+        FROM "stareau_principale".noeud_reseau AS n
+        WHERE ST_DWithin(n.geom, end_point, 0.1)
+          AND n.type_reseau <> 'aep'
+        ORDER BY n.geom <-> end_point, n.fid
+        LIMIT 1
+        ;
+    END IF;
+
+    -- downstream - update value
+    IF downstream_node IS NOT NULL THEN
+        IF raise_notice IN ('info', 'debug') THEN
+            RAISE NOTICE '% BEFORE canalisation % n° %, downstream_node NOT NULL : % % -> use it',
+                repeat('    ', pg_trigger_depth()::integer), TG_OP, NEW.fid, downstream_node.fid, downstream_node.id_noeud_reseau
+            ;
+        END IF;
+
+        -- Update the geometry
+        NEW.geom = ST_SetPoint(NEW.geom,  ST_NPoints(NEW.geom) - 1, downstream_node.geom);
+        -- Update the node ID in upstream attribute
+        NEW.noeudterminal = downstream_node.id_noeud_reseau;
+    ELSE
+        IF raise_notice IN ('info', 'debug') THEN
+            RAISE NOTICE '% BEFORE canalisation % n° %, downstream_node IS NULL',
+                repeat('    ', pg_trigger_depth()::integer), TG_OP, NEW.fid
+            ;
+        END IF;
+
+        -- Update the node ID in upstream attribute
+        NEW.noeudterminal = 'non_renseigne';
+    END IF;
+
+    RETURN NEW;
+END;
+$$;
+
+
+-- FUNCTION before_canalisation_insert_or_update()
+COMMENT ON FUNCTION stareau.before_canalisation_insert_or_update() IS
+'Fonction qui lie les canalisations aux noeuds de réseau en amont et en aval
+Si le point initial de la géometrie de la canalisation est à moins de 10cm d''un noeud de réseau,
+alors on lie la canalisation à ce noeud en modifiant la géométrie et le champ noeudinitial
+sinon la valeur ''non_renseigne'' est précisée dans le champs noeudinitial
+Si le point final de la géometrie de la canalisation est à moins de 10m d''un noeud de réseau,
+alors on lie la canalisation à ce noeud en modifiant la géométrie et le champ noeudterminal
+sinon la valeur ''non_renseigne'' est précisée dans le champs noeudterminal
+';
+
+-- after_noeud_reseau_insert_or_update()
+CREATE FUNCTION stareau.after_noeud_reseau_insert_or_update() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$
+DECLARE
+    is_aep boolean;
+    _set_config text;
+    raise_notice text;
+BEGIN
+    -- Trigger disabled by session variable
+    IF stareau.get_current_setting('stareau.graph.disable.trigger', '0', 'boolean')
+    THEN
+        RETURN NEW;
+    END IF;
+
+    -- Check if we must log
+    raise_notice = stareau.get_current_setting('stareau.graph.raise.notice', '0', 'boolean');
+
+    -- Do nothing if geometry has not changed
+    IF TG_OP = 'UPDATE' AND ST_Equals(NEW.geom, OLD.geom) THEN
+        IF raise_notice IN ('info', 'debug') THEN
+            RAISE NOTICE '% AFTER noeud_reseau % n° % UPDATE, NEW and OLD geom are equal',
+                repeat('    ', pg_trigger_depth()::integer), TG_OP, NEW.fid
+            ;
+        END IF;
+
+        RETURN NEW;
+    END IF;
+
+    -- is reseau AEP ?
+    is_aep = (NEW.type_reseau = 'aep');
+
+    -- Disable triggers
+    SELECT set_config('stareau.graph.disable.trigger', '1'::text, true)
+    INTO _set_config;
+
+    IF TG_OP = 'UPDATE' THEN
+        IF raise_notice IN ('info', 'debug') THEN
+            RAISE NOTICE '% AFTER noeud_reseau % n° % UPDATE, UPDATE linked canalisations',
+                repeat('    ', pg_trigger_depth()::integer), TG_OP, NEW.fid
+            ;
+        END IF;
+
+        IF is_aep THEN
+            UPDATE "stareau_principale".canalisation SET geom = ST_SetPoint(geom, 0, NEW.geom)
+            WHERE noeudinitial = NEW.id_noeud_reseau
+              AND type_reseau = 'aep';
+            UPDATE "stareau_principale".canalisation SET geom = ST_SetPoint(geom, ST_NPoints(geom) - 1, NEW.geom)
+            WHERE noeudterminal = NEW.id_noeud_reseau
+              AND type_reseau = 'aep';
+        ELSE
+            UPDATE "stareau_principale".canalisation SET geom = ST_SetPoint(geom, 0, NEW.geom)
+            WHERE noeudinitial = NEW.id_noeud_reseau
+              AND type_reseau <> 'aep';
+            UPDATE "stareau_principale".canalisation SET geom = ST_SetPoint(geom, ST_NPoints(geom) - 1, NEW.geom)
+            WHERE noeudterminal = NEW.id_noeud_reseau
+              AND type_reseau <> 'aep';
+        END IF;
+    END IF;
+
+    IF TG_OP = 'INSERT' THEN
+        IF raise_notice IN ('info', 'debug') THEN
+            RAISE NOTICE '% AFTER noeud_reseau % n° % INSERT, UPDATE canalisations links',
+                repeat('    ', pg_trigger_depth()::integer), TG_OP, NEW.fid
+            ;
+        END IF;
+
+        IF is_aep THEN
+            UPDATE "stareau_principale".canalisation
+               SET geom = ST_SetPoint(geom, 0, NEW.geom), noeudinitial = NEW.id_noeud_reseau
+            WHERE noeudinitial = 'non_renseigne'
+              AND ST_DWITHIN(geom, NEW.geom, 0.1)
+              AND ST_DWITHIN(ST_StartPoint(geom), NEW.geom, 0.1)
+              AND type_reseau = 'aep';
+            UPDATE "stareau_principale".canalisation
+               SET geom = ST_SetPoint(geom, ST_NPoints(geom) - 1, NEW.geom), noeudterminal = NEW.id_noeud_reseau
+            WHERE noeudterminal = 'non_renseigne'
+              AND ST_DWITHIN(geom, NEW.geom, 0.1)
+              AND ST_DWITHIN(ST_EndPoint(geom), NEW.geom, 0.1)
+              AND type_reseau = 'aep';
+        ELSE
+            UPDATE "stareau_principale".canalisation
+               SET geom = ST_SetPoint(geom, 0, NEW.geom), noeudinitial = NEW.id_noeud_reseau
+            WHERE noeudinitial = 'non_renseigne'
+              AND ST_DWITHIN(geom, NEW.geom, 0.1)
+              AND ST_DWITHIN(ST_StartPoint(geom), NEW.geom, 0.1)
+              AND type_reseau <> 'aep';
+            UPDATE "stareau_principale".canalisation
+               SET geom = ST_SetPoint(geom, ST_NPoints(geom) - 1, NEW.geom), noeudterminal = NEW.id_noeud_reseau
+            WHERE noeudterminal = 'non_renseigne'
+              AND ST_DWITHIN(geom, NEW.geom, 0.1)
+              AND ST_DWITHIN(ST_EndPoint(geom), NEW.geom, 0.1)
+              AND type_reseau <> 'aep';
+        END IF;
+    END IF;
+
+    -- Re-enable triggers
+    SELECT set_config('stareau.graph.disable.trigger', '0'::text, true)
+    INTO _set_config;
+
+    RETURN NEW;
+END;
+$$;
+
+-- FUNCTION after_noeud_reseau_insert_or_update()
+COMMENT ON FUNCTION stareau.after_noeud_reseau_insert_or_update() IS
+'Fonction qui modifie les canalisations afin de les lier aux noeuds de réseau ajoutés ou modifiés
+Si le noeud modifié est référencé dans le champ noeudinitial ou noeudterminal d''une canalisation,
+alors la géométrie de la canalisation est modifiée pour maintenir le lien géographique avec le noeud de réseau
+Si le noeud ajouté se trouve à moins de 10 cm du point initial ou final de la géométrie d''une canalisation,
+et que le champ noeudinitial ou noeudterminal de la canalisation est renseigné avec la valeur ''non_renseigne'',
+alors la géométrie de la canalisation est modifiée pour créer le lien géographique avec le noeud de réseau
+et le champ noeudinitial ou noeudterminal est renseigné avec l''identifiant du noeud de réseau.
+';
+
+-- after_noeud_reseau_delete()
+CREATE FUNCTION stareau.after_noeud_reseau_delete() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$
+DECLARE
+    is_aep boolean;
+    _set_config text;
+    raise_notice text;
+BEGIN
+    -- Trigger disabled by session variable
+    IF stareau.get_current_setting('stareau.graph.disable.trigger', '0', 'boolean')
+    THEN
+        RETURN OLD;
+    END IF;
+
+    -- Check if we must log
+    raise_notice = stareau.get_current_setting('stareau.graph.raise.notice', '0', 'boolean');
+
+    IF raise_notice IN ('info', 'debug') THEN
+        RAISE NOTICE '% AFTER noeud_reseau % n° % DELETE, UPDATE canalisations links',
+            repeat('    ', pg_trigger_depth()::integer), TG_OP, OLD.fid
+        ;
+    END IF;
+
+    UPDATE "stareau_principale".canalisation
+        SET noeudinitial = 'non_renseigne'
+    WHERE noeudinitial = OLD.id_noeud_reseau;
+
+    UPDATE "stareau_principale".canalisation
+        SET noeudterminal = 'non_renseigne'
+    WHERE noeudterminal = OLD.id_noeud_reseau;
+
+    RETURN OLD;
+END;
+$$;
+
+-- FUNCTION after_noeud_reseau_insert_or_update()
+COMMENT ON FUNCTION stareau.after_noeud_reseau_insert_or_update() IS
+'Fonction qui modifie les canalisations afin de modifier les champs noeudinitial et noeudterminal
+en assignant la valeur ''non_renseigne'' pour le noeud supprimé.
+';
+
 --
 -- PostgreSQL database dump complete
 --
-
-
-
