94c04e0b178c57ca3665cdbaeae9dd1733e7b4f2
elioooooott <eyvetot@3liz.com>
2026-07-16 16:53:26 +0200

    Add database tests installation files

---
+++ tests/data/install-version-2/csv/StaR-Eau/README.md
@@ -0,0 +1,5 @@
+# StaR-Eau
+
+Files from https://github.com/cnigfr/StaR-Eau/tree/main/Fichiers%20techniques/listes%20valeurs/brutes_avant_import
+
+Designed to be imported in database with https://github.com/cnigfr/StaR-Eau/blob/main/Fichiers%20techniques/base%20postgis/09-script_creation_liste_valeurs.py
---
+++ tests/data/install-version-2/sql/stareau/40_INDEX.sql
@@ -0,0 +1,30 @@
+--
+-- PostgreSQL database dump
+--
+
+
+
+
+
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+
+
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+-- stareau_aep_vertex_geom_idx
+CREATE INDEX stareau_aep_vertex_geom_idx ON stareau.aep_vertex USING gist (geom);
+
+-- stareau_aep_edge_geom_idx
+CREATE INDEX stareau_aep_edge_geom_idx ON stareau.aep_edge USING gist (geom);
+
+--
+-- PostgreSQL database dump complete
+--
